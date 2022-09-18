3d17a031b19dcdce7cc62cccdfb95ed56fb3cc59
Viraj Shah <veer15102003@gmail.com>
2022-09-18 14:32:31 +0530

    Intial Commit || Added React + tailwind + vite

---
+++ vite-react-tailwind/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+	return (
+		<div className='flex items-center justify-center bg-black text-white w-screen h-screen'>
+			<div className='font-bold text-5xl'>
+				This is made using vite + react + tailwind
+			</div>
+		</div>
+	)
+}
+
+export default App

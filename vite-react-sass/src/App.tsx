f334179a8c7cdcc378b76e7f22aee0cf27323f72
Viraj Shah <veer15102003@gmail.com>
2022-09-18 14:58:12 +0530

    Added React + sass + vite

---
+++ vite-react-sass/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../../vite-react-sass/styles/App.scss"
+
+const App = () => {
+	return (
+		<div className='container'>
+			<h1>This is made using React + Vite + Sass</h1>
+		</div>
+	)
+}
+
+export default App
